e834b754f9c717f2e4d85295c3a1bf62509eb130
Leo <leoocast@gmail.com>
2025-04-29 13:26:38 -0700

    Upgrade ui

--- scripts/game/UpgradeOptions.gd
+++ scripts/game/UpgradeOptions.gd
@@ -1,2 +1,26 @@
 extends Panel
 
+var panelHoverStyle = theme.get_stylebox("hover", "Panel")
+var panelNormalStyle = theme.get_stylebox("normal", "Panel")
+
+func _on_mouse_entered() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.08, 1.08), 0.1)
+	add_theme_stylebox_override("panel", panelHoverStyle)
+
+func _on_mouse_exited() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
+	add_theme_stylebox_override("panel", panelNormalStyle)
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector2(.95, .95), 0.1)
+		add_theme_stylebox_override("panel", panelNormalStyle)
+		print("Click en upgrade")
+
+	if event is InputEventMouseButton and not event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
