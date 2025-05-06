6beeb95ecf31bd311f627f342dbdfd5da5767b9a
Leo <leoocast@gmail.com>
2025-05-05 17:20:41 -0700

    Weapon as node

---
+++ scripts/player/upgrades/UpgradesController.gd
@@ -0,0 +1,22 @@
+class_name UpgradesController
+extends Node
+
+#Preload
+const MOVEMENT_SPEED_UPGRADE = preload("res://scripts/player/upgrades/MovementSpeedUpgrade.gd")
+const ATTACK_DAMAGE_UPGRADE = preload("res://scripts/player/upgrades/AttackDamageUpgrade.gd")
+const ATTACK_sPEED_UPGRADE = preload("res://scripts/player/upgrades/AttackSpeedUpgrade.gd")
+
+#Internal
+var upgrades: Array[Upgrade]
+
+func _ready():
+	upgrades = [
+		MOVEMENT_SPEED_UPGRADE.new(),
+		ATTACK_DAMAGE_UPGRADE.new(),
+		ATTACK_sPEED_UPGRADE.new(),
+	]
+
+func getRandomUpgrades(count: int) -> Array[Upgrade]:
+	var shuffled = upgrades.duplicate()
+	shuffled.shuffle()
+	return shuffled.slice(0, count)
